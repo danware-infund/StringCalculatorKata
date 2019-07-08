acd84058b7931f7038bd72494f909f1d8176362d
Dan Ware <scandal.uk@gmail.com>
2019-07-08 21:46:43 +0100

    Passing test for multiple numbers

--- StringCalculatorTests/StringCalculator.cs
+++ StringCalculatorTests/StringCalculator.cs
@@ -7,7 +7,12 @@
             var total = 0;
 
             if (!string.IsNullOrEmpty(numbers))
-                total = int.Parse(numbers);
+            {
+                if (numbers.IndexOf(',') > 0)
+                    foreach (string number in numbers.Split(','))
+                        total += int.Parse(number);
+                else total = int.Parse(numbers);
+            }
 
             return total;
         }
