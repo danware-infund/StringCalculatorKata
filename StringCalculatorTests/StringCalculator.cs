4722711edf5e5be5ef2734538e74e15c5df9790d
Dan Ware <scandal.uk@gmail.com>
2019-07-08 21:58:53 +0100

    Refactored

--- StringCalculatorTests/StringCalculator.cs
+++ StringCalculatorTests/StringCalculator.cs
@@ -9,19 +9,22 @@
             var total = 0;
 
             if (!string.IsNullOrEmpty(numbers))
-            {
-                var customDelimiter = "";
-                if (numbers.IndexOf("\\") == 0)
-                {
-                    customDelimiter = string.Format("\\{0}", numbers.Substring(1, (numbers.IndexOf('\n') - 1)));
-                    numbers = numbers.Substring(numbers.IndexOf("\n") + 1);
-                }
-
-                string[] numberArray = Regex.Split(numbers, $"[,\n{customDelimiter}]");
-                foreach (string number in numberArray)
+                foreach (string number in SplitNumbers(numbers))
                     total += int.Parse(number);
-            }
+
             return total;
+        }
+
+        private static string[] SplitNumbers(string numbers)
+        {
+            var delimiter = ",";
+            if (numbers.IndexOf("\\") == 0)
+            {
+                delimiter = string.Format("\\{0}", numbers.Substring(1, (numbers.IndexOf('\n') - 1)));
+                numbers = numbers.Substring(numbers.IndexOf("\n") + 1);
+            }
+
+            return Regex.Split(numbers, $"[\n{delimiter}]");
         }
     }
 }
